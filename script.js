cf785cc24a1f3733ecfbeeb66fce54b6fbd2bb81
robloxkid69 <44788378+robloxkid69@users.noreply.github.com>
2018-12-12 17:47:11 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+$(".tee2").click(function(){
+    $(".tee").slideToggle();
+});
+$(".hoodie2").click(function(){
+    $(".hoodie").fadeToggle();
+});
+$(".hat2").click(function() {
+    $(".hat").toggle();
+});
